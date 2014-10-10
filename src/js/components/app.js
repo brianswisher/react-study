fea1e68f3edbd9c5d2a743298a810cb1dc5fd047
brianswisher <bswisher@gmail.com>
2014-10-10 15:26:35 -0700

    route

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -2,20 +2,29 @@
  * @jsx React.DOM
  */
 
- var React = require('react');
- var Catalog = require('./catalog/app-catalog.js');
- var Cart = require('./cart/app-cart.js');
- var APP =
-  React.createClass({
-    render:function(){
-      return (
-        <div>
-        <h1>Lets shop</h1>
-        <Catalog />
-        <h1>Cart</h1>
-        <Cart />
-        </div>
-      )
-    }
-  });
+var React = require('react');
+var Catalog = require('./catalog/app-catalog.js');
+var Cart = require('./cart/app-cart.js');
+var Router = require('react-router-component');
+var CatalogDetail = require('./product/app-catalogdetail.js');
+var Template = require('./app-template.js');
+
+var Locations = Router.Locations;
+var Location = Router.Location;
+
+
+var APP =
+React.createClass({
+  render:function(){
+    return (
+      <Template>
+        <Locations>
+          <Location path="/" handler={Catalog}></Location>
+          <Location path="/cart" handler={Cart}></Location>
+          <Location path="/item/:item" handler={CatalogDetail}></Location>
+        </Locations>
+      </Template>
+    )
+  }
+});
 module.exports = APP;
--- src/js/components/header/app-cartsummary.js
+++ src/js/components/header/app-cartsummary.js
@@ -1,8 +1,18 @@
 /** @jsx React.DOM */
+var React = require('react');
+var Link = require('react-router-component').Link;
 var CartSummary =
   React.createClass({
     render:function(){
-      return ()
+      return (
+        <div>
+          <Link
+            href="/cart"
+            className="btn btn-success">
+            Cart Items: QTY / 1COST
+            </Link>
+        </div>
+      )
     }
   });
 module.exports = CartSummary;
